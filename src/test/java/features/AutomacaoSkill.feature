7feaee96c52d57cc84283ff79c7ec23760e83c86
Milton Denys <milton.silva17@fatec.sp.gov.br>
2023-03-27 23:54:39 -0300

    template basico desing patter page objects

---
+++ src/test/java/features/AutomacaoSkill.feature
@@ -0,0 +1,8 @@
+#Author: Milton Denys
+
+@inm_test
+Feature: Validar pagina de aulas Skill
+
+  Scenario: Validar se é possivel cadastrar um aluno no site da skill
+
+    Given que preencho o campo de nome ""
---
+++ src/test/java/steps/AutomacaoSkillSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class AutomacaoSkillSteps {
+    @Given("que preencho o campo de nome {string}")
+    public void preencherCampoNome(String nome) {
+
+    }
+
+}
